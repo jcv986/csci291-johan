5a3c13360855b6c67da79c2bfaeb0fd4ff805482
jcv986 <jcv986@uowmail.edu.au>
2024-09-25 16:28:23 +0400

    Add files via upload

---
+++ Untitled-johan.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define e_c 8;
+#define e_w 30;
+#define e_m 0;
+#define e_chocolate 0;
+#define e_p 3.5;
+
+#define c_c 8;
+#define c_w 30;
+#define c_m 70;
+#define c_chocolate 0;
+#define c_p 4.5;
+
+#define m_c 8;
+#define m_w 39;
+#define m_m 169;
+#define m_chocolate 30;
+#define m_p 5.5;
+
+#define adminpassword 2121
+#define low_threshold_beans 8
+#define low_threshold_water 30
+#define low_threshold_water 39
+#define low_threshold_milk 70
+#define low_threshold_milk 160
+#define low_threshold_chocolate 30
+
